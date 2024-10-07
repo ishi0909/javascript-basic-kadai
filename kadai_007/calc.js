63a4ac22080bc3b14559594636165783d228f54e
ishi0909 <tororosoba41@icloud.com>
2024-10-07 21:48:01 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,14 +1,14 @@
-let num = 3;
+let num = Math.floor(Math.random()*91);
 console.log(num);
 
-if (num===3) {
-  console.log("３の倍数です");
+if (num % 3===0 && num % 5===0) {
+  console.log("３と５の倍数です")
 }
-else if (num===5) {
+else if (num % 5===0) {
   console.log("５の倍数です");
 }
-else if (num===15) {
-  console.log("３と５の倍数です")
+else if (num % 3===0) {
+  console.log("３の倍数です");
 }
 else {
   console.log(num)
