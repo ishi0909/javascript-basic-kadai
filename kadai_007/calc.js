b4efb1561067057bc460295f7d7e65ed70347958
ishi0909 <tororosoba41@icloud.com>
2024-10-07 15:02:55 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+let num = 3;
+console.log(num);
+
+if (num===3) {
+  console.log("３の倍数です");
+}
+else if (num===5) {
+  console.log("５の倍数です");
+}
+else if (num===15) {
+  console.log("３と５の倍数です")
+}
+else {
+  console.log(num)
+}
+
