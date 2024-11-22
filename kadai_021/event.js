c3ec1af74ccb644f181b3ccbe96dcaf1c3fc015d
ishi0909 <tororosoba41@icloud.com>
2024-11-22 14:11:50 +0900

    Delete kadai_021/event.js

--- kadai_021/event.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-
-btn.addEventListener('click' , () => {
-  setTimeout(() => {
-    console.log(text);
-  }, 2000);
-})
